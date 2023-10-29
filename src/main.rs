4acd900c33ba26034d438c94e10942cf534c7f94
Simon Sawert <simon@sawert.se>
2023-10-29 23:36:11 +0100

    Working calculator

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,113 @@
-#[macro_use]
-extern crate pest_derive;
-
-use std::io::Write;
-
-use pest::iterators::Pair;
+use pest::iterators::Pairs;
+use pest::pratt_parser::PrattParser;
 use pest::Parser;
+use std::io::{self, BufRead};
 
-#[derive(Parser)]
+#[derive(pest_derive::Parser)]
 #[grammar = "calculator.pest"]
 pub struct CalculatorParser;
 
-fn main() -> anyhow::Result<()> {
-    print!("Enter an expression: ");
-    std::io::stdout().flush()?;
+lazy_static::lazy_static! {
+    static ref PRATT_PARSER: PrattParser<Rule> = {
+        use pest::pratt_parser::{Assoc::*, Op};
+        use Rule::*;
 
-    let mut buffer = String::new();
-    std::io::stdin().read_line(&mut buffer)?;
-
-    let mut pairs = CalculatorParser::parse(Rule::expression, &buffer)?;
-    parse(pairs.next().ok_or_else(|| anyhow::anyhow!("no pairs"))?);
-
-    Ok(())
+        // Precedence is defined lowest to highest
+        PrattParser::new()
+            // Addition and subtract have equal precedence
+            .op(Op::infix(add, Left) | Op::infix(subtract, Left))
+            .op(Op::infix(multiply, Left) | Op::infix(divide, Left) | Op::infix(modulo, Left) | Op::infix(pow, Left))
+            .op(Op::prefix(unary_minus))
+    };
 }
 
-fn parse(pair: Pair<Rule>) {
-    match pair.as_rule() {
-        Rule::expression => {
-            println!("{:#?}", pair);
+#[derive(Debug)]
+pub enum Op {
+    Add,
+    Subtract,
+    Multiply,
+    Divide,
+    Modulo,
+    Pow,
+}
+
+#[derive(Debug)]
+pub enum Expr {
+    Number(f64),
+    UnaryMinus(Box<Expr>),
+    BinOp {
+        lhs: Box<Expr>,
+        op: Op,
+        rhs: Box<Expr>,
+    },
+}
+
+impl Expr {
+    fn eval(&self) -> f64 {
+        match self {
+            Self::Number(i) => *i,
+            Self::UnaryMinus(expr) => -(expr.eval()),
+            Self::BinOp { lhs, op, rhs } => {
+                let lhs = lhs.eval();
+                let rhs = rhs.eval();
+
+                match op {
+                    Op::Add => lhs + rhs,
+                    Op::Subtract => lhs - rhs,
+                    Op::Multiply => lhs * rhs,
+                    Op::Divide => lhs / rhs,
+                    Op::Modulo => lhs % rhs,
+                    Op::Pow => lhs.powf(rhs),
+                }
+            }
         }
-        _ => unreachable!(),
     }
+}
+
+pub fn parse_expr(pairs: Pairs<Rule>) -> Expr {
+    PRATT_PARSER
+        .map_primary(|primary| match primary.as_rule() {
+            Rule::number => Expr::Number(primary.as_str().parse::<f64>().unwrap()),
+            Rule::expr => parse_expr(primary.into_inner()),
+            rule => unreachable!("Expr::parse expected atom, found {:?}", rule),
+        })
+        .map_infix(|lhs, op, rhs| {
+            let op = match op.as_rule() {
+                Rule::add => Op::Add,
+                Rule::subtract => Op::Subtract,
+                Rule::multiply => Op::Multiply,
+                Rule::divide => Op::Divide,
+                Rule::modulo => Op::Modulo,
+                Rule::pow => Op::Pow,
+                rule => unreachable!("Expr::parse expected infix operation, found {:?}", rule),
+            };
+
+            Expr::BinOp {
+                lhs: Box::new(lhs),
+                op,
+                rhs: Box::new(rhs),
+            }
+        })
+        .map_prefix(|op, rhs| match op.as_rule() {
+            Rule::unary_minus => Expr::UnaryMinus(Box::new(rhs)),
+            _ => unreachable!(),
+        })
+        .parse(pairs)
+}
+
+fn main() -> io::Result<()> {
+    for line in io::stdin().lock().lines() {
+        match CalculatorParser::parse(Rule::equation, &line?) {
+            Ok(mut pairs) => {
+                let expr = parse_expr(pairs.next().unwrap().into_inner());
+                println!("Parsed: {:#?}", expr);
+                println!("Evaluated: {}", expr.eval());
+            }
+            Err(e) => {
+                eprintln!("Parse failed: {:?}", e);
+            }
+        }
+    }
+
+    Ok(())
 }
